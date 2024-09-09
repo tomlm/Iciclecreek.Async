dd57afd1ae4de39f84dfd50b65677bedf01f4973
Tom Laird-McConnell <thermous@iciclecreek.com>
2024-09-09 11:58:15 -0700

    add library

---
+++ source/Iciclecreek.Async.Tests/ExtensionTests.cs
@@ -0,0 +1,117 @@
+using System.Diagnostics;
+
+namespace Iciclecreek.Async.Tests;
+
+[TestClass]
+public class ExtensionTests
+{
+
+    public class Item
+    {
+        public int Value { get; set; }
+        public bool IsEven { get; set; }
+    }
+
+
+    [TestMethod]
+    public void SelectParallelAsync_Enumerable()
+    {
+        Random rnd = new Random();
+        var count = 20;
+        var numbers = Enumerable.Range(0, count);
+
+        Stopwatch sw = new Stopwatch();
+        sw.Start();
+
+        foreach (var result in numbers
+            .SelectParallelAsync(action, 10)
+            .Where(item => item.IsEven))
+        {
+            Assert.AreEqual(true, result.IsEven);
+        }
+        sw.Stop();
+        Assert.AreEqual(2, sw.Elapsed.Seconds);
+
+        sw.Reset();
+        sw.Start();
+
+        foreach (var result in numbers
+            .SelectParallelAsync(action)
+            .Where(item => item.IsEven))
+        {
+            Assert.AreEqual(true, result.IsEven);
+        }
+        sw.Stop();
+        Assert.AreEqual(1, sw.Elapsed.Seconds);
+    }
+
+    [TestMethod]
+    public void SelectParallelAsync_Parallel()
+    {
+        Random rnd = new Random();
+        var count = 20;
+        var numbers = Enumerable.Range(0, count);
+
+        Stopwatch sw = new Stopwatch();
+        sw.Start();
+
+        foreach (var result in numbers
+            .AsParallel()
+            .WithDegreeOfParallelism(10)
+            .SelectParallelAsync(action)
+            .Where(item => item.IsEven))
+        {
+            Assert.AreEqual(true, result.IsEven);
+        }
+
+        sw.Stop();
+        Assert.AreEqual(2, sw.Elapsed.Seconds);
+
+        sw.Reset();
+        sw.Start();
+
+        foreach (var result in numbers
+            .AsParallel()
+            .SelectParallelAsync(action, 10)
+            .Where(item => item.IsEven))
+        {
+            Assert.AreEqual(true, result.IsEven);
+        }
+
+        sw.Stop();
+        Assert.AreEqual(2, sw.Elapsed.Seconds);
+    }
+
+    [TestMethod]
+    public void WaitAsync_Enumerable()
+    {
+        Random rnd = new Random();
+        var count = 20;
+        var numbers = Enumerable.Range(0, count);
+
+        Stopwatch sw = new Stopwatch();
+        sw.Start();
+
+        foreach (var result in numbers
+            .Select(action)
+            .WaitAll()
+            .Where(item => item.IsEven))
+        {
+            Assert.AreEqual(true, result.IsEven);
+        }
+        sw.Stop();
+        Assert.AreEqual(1, sw.Elapsed.Seconds);
+    }
+
+    private static async Task<Item> action(int item, int pos)
+    {
+        await Task.Delay(1000);
+        return new Item
+        {
+            Value = item,
+            IsEven = item % 2 == 0
+        };
+    }
+
+
+}
---
+++ source/Iciclecreek.Async/Extensions.cs
@@ -0,0 +1,44 @@
+﻿using System.Reflection;
+
+namespace Iciclecreek.Async;
+
+public static class Extensions
+{
+    public static IEnumerable<TResult> SelectParallelAsync<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, int, Task<TResult>> selector, int maxParallel = int.MaxValue)
+    {
+        if (source is ParallelQuery<TSource>)
+        {
+            object? settings = source.GetType().GetProperty("SpecifiedQuerySettings", BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(source);
+            if (settings != null)
+                maxParallel = (int?)settings.GetType().GetProperty("DegreeOfParallelism", BindingFlags.NonPublic | BindingFlags.Instance)?.GetValue(settings) ?? maxParallel;
+        }
+        SemaphoreSlim semaphore = new SemaphoreSlim(maxParallel);
+        var tasks = new List<Task<TResult>>();
+        int index = -1;
+        foreach (var item in source)
+        {
+            index = index + 1;
+            tasks.Add(semaphore.WaitAsync()
+                .ContinueWith(t =>
+                {
+                    return selector(item, index)
+                        .ContinueWith(t =>
+                        {
+                            semaphore.Release();
+                            return t.Result;
+                        });
+                }).Unwrap());
+        }
+        Task.WaitAll(tasks.ToArray());
+        return tasks.Select(t => t.Result);
+    }
+
+
+    public static IEnumerable<TSource> WaitAll<TSource>(this IEnumerable<Task<TSource>> source)
+    {
+        var list = source.ToArray();
+        Task.WaitAll(list);
+        return list.Select(t => t.Result);
+    }
+
+}
